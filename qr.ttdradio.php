76b0c12d5a26e19b547b2bd57a25a11a98ceb8a0
Trần Trung Dũng <137852406+DrTrungDung@users.noreply.github.com>
2026-03-11 16:49:09 +0700

    Update qr.ttdradio.php

--- qr.ttdradio.php
+++ qr.ttdradio.php
@@ -1,9 +1,9 @@
 <?php
 $user_agent = $_SERVER['HTTP_USER_AGENT'];
 $user_agent = strtolower($user_agent);
-$ios_link = "https://your-ios-link.com";
-$android_link = "https://your-android-link.com";
-$default_link = "https://website-chinh.com";
+$ios_link = "http://drtrungdung.io.vn/iptv/ttdradio-ios.html";
+$android_link = "http://drtrungdung.io.vn/iptv/ttdradio-adr.html";
+$default_link = "http://drtrungdung.io.vn/iptv/ttdradio.html";
 if (strpos($user_agent, 'iphone') !== false || 
     strpos($user_agent, 'ipad') !== false || 
     strpos($user_agent, 'ipod') !== false) {header("Location: $ios_link");} 
